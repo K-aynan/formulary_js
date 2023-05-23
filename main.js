e56acdf3d2e46e7ab6e5d95233c3f4e139622e4b
kaynansn11@gmail.com <kaynansn11@gmail.com>
2023-05-23 09:35:43 -0300

    maybe fourth trial

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-function validarFormulario() {
+const form = document.querySelector("form");
+
+form.addEventListener("submit",validarFormulario);
+
+function validarFormulario(e) {
+  e.preventDefault();
+
   var numeroA = parseInt(document.getElementById("numeroA").value);
   var numeroB = parseInt(document.getElementById("numeroB").value);
 
   if (numeroB > numeroA) {
-    alert("Formulário válido!");
-    return true;
+    document.querySelector(".resultadoB").innerHTML = '<p>Seu formulário é válido! Campo B é maior que Campo A.</p>';
   } else {
-    alert("Número B deve ser maior que número A!");
-    return false;
+    document.querySelector(".resultadoA").innerHTML = '<p>Infelizmente Campo A é maior que Campo B e seu formulário não é válido.</p>';
   }
+
+  document.getElementById("numeroA").value = "";
+  document.getElementById("numeroB").value = "";
 }
